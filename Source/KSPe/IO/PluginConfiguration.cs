dbac24573d2e0da310233676b6396787113a54b5
Lisias T <me@lisias.net>
2018-08-16 15:27:18 -0300

    Project Normalizing

---
+++ Source/KSPe/IO/PluginConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using KSP;
+using System;
+using System.IO;
+
+namespace KSPe.IO
+{
+	public class PluginConfiguration : KSP.IO.PluginConfiguration
+	{
+		protected PluginConfiguration(string pathToFile) : base(pathToFile) { }
+
+		public static new PluginConfiguration CreateForType<T>(Vessel flight)
+		{
+			Type target = typeof(T);
+			string fn = Path.Combine(KSPUtil.ApplicationRootPath, "PluginData");
+			fn = Path.Combine(fn, target.Namespace);
+
+			// TODO: Checar como funciona o Vessel. Possivelmente nesse caso o XML deve estar no savegame!
+			fn = Path.Combine(fn,
+							  (flight ? flight.GetName() + "." + target.Name : target.Name) + ".xml"
+			);
+
+			return new PluginConfiguration(fn);
+		}
+
+		public static PluginConfiguration CreateForType<T>()
+		{
+			return CreateForType<T>((Vessel)null);
+		}
+	}
+}
