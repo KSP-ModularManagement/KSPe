2d0cb821fbea83683858437b35a07381c71d2e18
Lisias T <me@lisias.net>
2018-09-30 08:15:51 -0300

    Adjusts on PluginConfiguration

--- Source/KSPe/IO/PluginConfiguration.cs
+++ Source/KSPe/IO/PluginConfiguration.cs
@@ -8,18 +8,23 @@ namespace KSPe.IO
 	{
 		protected PluginConfiguration(string pathToFile) : base(pathToFile) { }
 
-		public static new PluginConfiguration CreateForType<T>(Vessel flight)
+		public static PluginConfiguration CreateForType<T>(string filename)
 		{
 			Type target = typeof(T);
 			string fn = Path.Combine(KSPUtil.ApplicationRootPath, "PluginData");
 			fn = Path.Combine(fn, target.Namespace);
+			if (!System.IO.Directory.Exists(fn))
+				System.IO.Directory.CreateDirectory(fn);
 
 			// TODO: Checar como funciona o Vessel. Possivelmente nesse caso o XML deve estar no savegame!
-			fn = Path.Combine(fn,
-							  (flight ? flight.GetName() + "." + target.Name : target.Name) + ".xml"
-			);
-
+			fn = Path.Combine(fn, filename);
 			return new PluginConfiguration(fn);
+		}
+
+		public static new PluginConfiguration CreateForType<T>(Vessel flight)
+		{
+			Type target = typeof(T);
+			return CreateForType<T>((flight ? flight.GetName() + "." + target.Name : target.Name) + ".xml");
 		}
 
 		public static PluginConfiguration CreateForType<T>()
