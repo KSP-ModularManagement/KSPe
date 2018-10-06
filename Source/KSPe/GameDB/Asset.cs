078a326215ad88e2b1817bd2ce7d4fa108308e30
Lisias T <me@lisias.net>
2018-10-06 14:04:57 -0300

    Adding GameDB namespace: utilities for abstracting PluginResources naming from the GameDatabase

---
+++ Source/KSPe/GameDB/Asset.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace KSPe.GameDB
+
+{
+	public static class Asset<T>
+	{
+		public static string Solve(string fn)
+		{
+			string r = KSPe.IO.File<T>.Asset.Solve(fn);
+			r = r.Substring(r.IndexOf("GameData/") + 9);
+			return r;
+		}
+	}
+}
