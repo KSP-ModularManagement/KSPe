c2e1dbc9515e7360ec8b3627f9472ec821ee5a2c
Lisias T <me@lisias.net>
2018-10-10 15:49:37 -0300

    Updating automation scripts

--- publish.sh
+++ publish.sh
@@ -4,4 +4,6 @@ source ./CONFIG.inc
 
 VERSIONFILE=$PACKAGE.version
 
-scp -i $SSH_ID ./GameData/net.lisias.ksp/$VERSIONFILE $SITE:/$TARGETPATH
+scp -i $SSH_ID "./GameData/$TARGETDIR/$VERSIONFILE" $SITE:/$TARGET_CONTENT_PATH
+scp -i $SSH_ID "./GameData/$TARGETDIR/README.md" $SITE:/${TARGET_CMS_PATH}${PACKAGE}.md
+#scp -i $SSH_ID "./PR_material/banner.jpg" $SITE:/${TARGET_CONTENT_PATH}PR_material/banner.jpg
