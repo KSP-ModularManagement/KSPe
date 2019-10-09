225736d1bdeb2603ec9171b259468e14bafa0401
Lisias T <me@lisias.net>
2019-10-09 19:57:59 -0300

    Adding installation checking on KSPe; Adding Common Diaglos FatalError on KSPe.UI.
    For TweakScale#76 ( https://github.com/net-lisias-ksp/TweakScale/issues/76 )

---
+++ Source/KSPe.UI/Common/Dialogs/FatalError.cs
@@ -0,0 +1,98 @@
+﻿/*
+	This file is part of KSPe, a component for KSP API Extensions/L
+	(C) 2018-19 Lisias T : http://lisias.net <support@lisias.net>
+
+	KSPe API Extensions/L is double licensed, as follows:
+
+	* SKL 1.0 : https://ksp.lisias.net/SKL-1_0.txt
+	* GPL 2.0 : https://www.gnu.org/licenses/gpl-2.0.txt
+
+	And you are allowed to choose the License that better suit your needs.
+
+	KSPe API Extensions/L is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	You should have received a copy of the SKL Standard License 1.0
+	along with KSPe API Extensions/L. If not, see <https://ksp.lisias.net/SKL-1_0.txt>.
+
+	You should have received a copy of the GNU General Public License 2.0
+	along with KSPe API Extensions/L. If not, see <https://www.gnu.org/licenses/>.
+
+*/
+using System;
+using UnityEngine;
+using KSPe.UI;
+
+namespace KSPe.Common.Dialogs
+{
+	public static class ShowStopperAlertBox
+	{
+		private static readonly string aMSG = "close KSP and then fix the problem described above";
+
+		private static readonly string MSG = @"{0}
+
+This is a Show Stopper problem. Your best line of action is to click the OK button to {1}.
+
+If you choose to ignore this message and click Cancel to proceed, be advised that your savegames can get corrupted at any time, even when things appear to work by now - and the salvage can be harder.
+
+Backup everything *NOW* if you choose to ignore this message and proceed - it's recommended to use S.A.V.E. to automate this task for you.";
+
+		public static void Show(KSPe.Util.AbstractException ex)
+		{
+			Show(ex.ToLongMessage());
+		}
+
+		public static void Show(KSPe.Util.AbstractException ex, string actionMessage, Action lambda)
+		{
+			Show(ex.ToLongMessage(), actionMessage, lambda);
+		}
+
+		public static void Show(string errorMessage)
+		{
+			Show(errorMessage, aMSG, Application.Quit);
+		}
+
+		public static void Show(string errorMessage, string actionMessage, Action lambda)
+		{
+			GameObject go = new GameObject("KSPe.Common.Diallgs.ShowStopperAlertBox");
+			MessageBox dlg = go.AddComponent<MessageBox>();
+
+			GUIStyle win = new GUIStyle("Window")
+			{
+				fontSize = 26,
+				fontStyle = FontStyle.Bold,
+				alignment = TextAnchor.UpperCenter,
+				wordWrap = false
+			};
+			win.normal.textColor = Color.red;
+			win.border.top = 36;
+
+			GUIStyle text = new GUIStyle("Label")
+			{
+				fontSize = 18,
+				fontStyle = FontStyle.Normal,
+				alignment = TextAnchor.MiddleLeft,
+				wordWrap = true
+			};
+			text.normal.textColor = Color.white;
+			text.padding.top = 8;
+			text.padding.bottom = text.padding.top;
+			text.padding.left = text.padding.top;
+			text.padding.right = text.padding.top;
+			{
+				Texture2D tex = new Texture2D(1, 1);
+				tex.SetPixel(0, 0, new Color(0f, 0f, 0f, 0.45f));
+				tex.Apply();
+				text.normal.background = tex;
+			}
+
+			dlg.Show(
+				"Houston, we have a Problem!",
+				String.Format(MSG, errorMessage, actionMessage),
+				lambda,
+				win, text
+			);
+		}
+	}
+}
---
+++ Source/KSPe/Util/Installation.cs
@@ -0,0 +1,118 @@
+﻿/*
+	This file is part of KSPe, a component for KSP API Extensions/L
+	(C) 2018-19 Lisias T : http://lisias.net <support@lisias.net>
+
+	KSPe API Extensions/L is double licensed, as follows:
+
+	* SKL 1.0 : https://ksp.lisias.net/SKL-1_0.txt
+	* GPL 2.0 : https://www.gnu.org/licenses/gpl-2.0.txt
+
+	And you are allowed to choose the License that better suit your needs.
+
+	KSPe API Extensions/L is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	You should have received a copy of the SKL Standard License 1.0
+	along with KSPe API Extensions/L. If not, see <https://ksp.lisias.net/SKL-1_0.txt>.
+
+	You should have received a copy of the GNU General Public License 2.0
+	along with KSPe API Extensions/L. If not, see <https://www.gnu.org/licenses/>.
+
+*/
+using System;
+using System.Linq;
+using SIO = System.IO;
+
+namespace KSPe.Util
+{
+	public static class Installaltion
+	{
+		public class InstallationException : AbstractException
+		{
+			internal InstallationException(string shortMessage, params object[] @params) : base(shortMessage, @params) { }
+		}
+
+		public class WrongDirectoryInstallationException : InstallationException
+		{
+			private static readonly string message = @"Unfortunately {0} is installed on the wrong Directory!
+
+It should be installed on {1} but it's currently installed on {2} ! Delete the later and be sure to install {0} on the former.";
+			private static readonly string shortMessage = "{0} should be installed on [{1}], not on [{2}].";
+
+			public readonly string name;
+			public readonly string intendedPath;
+			public readonly string installedDllPath;
+
+			internal WrongDirectoryInstallationException(string name, string intendedPath, string installedDllPath) : base(shortMessage, name, intendedPath, installedDllPath)
+			{
+				this.name = name;
+				this.intendedPath = intendedPath;
+				this.installedDllPath = installedDllPath;
+			}
+
+			public override string ToLongMessage()
+			{
+				return string.Format(message, name, intendedPath, installedDllPath);
+			}
+		}
+
+		public static void Check<T>(string name, string vendor = null)
+		{
+			CheckForWrongDirectoy(typeof(T), name, vendor);
+		}
+
+		public static void Check<T>(System.Type versionClass)
+		{
+			string name = typeof(T).Namespace;
+			try
+			{
+				name = versionClass.GetField("Namespace").GetValue(null).ToString();
+			}
+			catch (ArgumentNullException) { }
+			catch (NotSupportedException) { }
+			catch (FieldAccessException) { }
+			catch (ArgumentException) { }
+			catch (NullReferenceException) { }
+
+			string vendor = null;
+			try
+			{
+				vendor = versionClass.GetField("Vendor").GetValue(null).ToString();
+			}
+			catch (ArgumentNullException) { }
+			catch (NotSupportedException) { }
+			catch (FieldAccessException) { }
+			catch (ArgumentException) { }
+			catch (NullReferenceException) { }
+
+			Check<T>(name, vendor);
+		}
+
+		public static void Check<T>()
+		{
+			Type versionClass = (from assembly in AppDomain.CurrentDomain.GetAssemblies()
+					from tt in assembly.GetTypes()
+					where tt.Namespace == typeof(T).Namespace && tt.Name == "Version"
+					select tt).FirstOrDefault();
+			Check<T>(versionClass);
+		}
+
+		private static void CheckForWrongDirectoy(Type type, string name, string vendor)
+		{
+			string kspRoot = SIO.Path.GetFullPath(KSPUtil.ApplicationRootPath);
+
+			string intendedPath = SIO.Path.Combine(kspRoot, "GameData");
+			if (null != vendor)	intendedPath = SIO.Path.Combine(intendedPath, vendor);
+			intendedPath = SIO.Path.Combine(intendedPath, name) + "/";
+			int dashCount = intendedPath.Length - intendedPath.Replace("/", "").Length;
+
+			string installedDllPath =  SIO.Path.GetDirectoryName(SIO.Path.GetFullPath(type.Assembly.Location)) + "/";
+			installedDllPath = string.Join("/", installedDllPath.Split('/').Take(dashCount).ToArray()) + "/";
+
+			if (installedDllPath.StartsWith(intendedPath)) return;
+
+			throw new WrongDirectoryInstallationException(name, intendedPath, installedDllPath);
+		}
+	}
+}
