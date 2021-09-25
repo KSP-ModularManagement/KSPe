4fcee9d8c3b689af3181fdf45d2dfbc4ff351285
Lisias T <me@lisias.net>
2021-09-25 13:48:52 -0300

    Automation scripts

--- clean.sh
+++ clean.sh
@@ -5,12 +5,20 @@ source ./CONFIG.inc
 clean() {
 	local DLL=$1.dll
 
-	rm -f "./GameData/$DLL"
-	rm -f "./GameData/$TARGETBINDIR/$DLL"
-	rm -f "./GameData/$TARGETBINDIR/PluginData/$DLL"
+	for d in . ${KSP_DEV} ; do
+		rm -f "${d}/GameData/$DLL"
+		rm -f "${d}/GameData/$TARGETBINDIR/$DLL"
+		rm -f "${d}/GameData/$TARGETBINDIR/PluginData/$DLL"
+		for dd in $PD_SUB_DIRS ; do
+			rm -f "${d}/GameData/$TARGETBINDIR/PluginData/${dd}/$DLL"
+		done
+	done
+}
+
+clean_lib() {
+	local DLL=$1.dll
+
 	rm -f "$LIB/$DLL"
-	rm -f "${KSP_DEV}/GameData/$DLL"
-	rm -f "${KSP_DEV}/GameData/$TARGETBINDIR/PluginData/$DLL"
 }
 
 VERSIONFILE=$PACKAGE.version
@@ -22,7 +30,9 @@ rm -f "./GameData/$TARGETDIR/CHANGE_LOG.md"
 rm -f "./GameData/$TARGETDIR/README.md"
 rm -f "./GameData/$TARGETDIR/LICENSE*"
 find "./GameData/$TARGETBINDIR" -name "KSPe.Light.*.dll" -delete
-for dll in $DLLS 000_KSPe ; do
+for dll in 000_KSPe $DLLS $PD_DLLS KSPe.UI ; do
     clean $dll
 done
-
+for dll in $LIB_DLLS ; do
+    clean_lib $dll
+done
--- deploy.sh
+++ deploy.sh
@@ -3,34 +3,84 @@
 source ./CONFIG.inc
 
 check() {
-	mkdir -p "./GameData/$TARGETDIR/Plugins/PluginData"
-	if [ ! -d "./GameData/$TARGETBINDIR/" ] ; then
-		rm -f "./GameData/$TARGETBINDIR/"
-		mkdir -p "./GameData/$TARGETBINDIR/"
+	for d in . $KSP_DEV ; do
+		if [ ! -d "${d}/GameData/${TARGETBINDIR}/" ] ; then
+			rm -f "${d}/GameData/${TARGETBINDIR}/"
+		fi
+		mkdir -p "${d}/GameData/${TARGETBINDIR}"
+		if [[ ! -z "${PD_DLLS}" ]] ; then
+			mkdir -p "${d}/GameData/${TARGETBINDIR}/PluginData/"
+		fi
+		if [[ ! -z "${PD_SUB_DIRS}" ]] ; then
+			for dd in ${PD_SUB_DIRS} ; do
+				mkdir -p "${d}/GameData/${TARGETBINDIR}/PluginData/${dd}/"
+			done
+		fi
+		rm -f ${d}/GameData/${TARGETBINDIR}/KSPe.Light.*
+	done
+
+	if [[ -d "./bin/Release" && -d "./bin/Debug" ]] ; then
+		echo "Conflicting Release and Debug dirs. Clean and rebuild!"
+		exit -1
 	fi
-	rm -f ./GameData/$TARGETBINDIR/KSPe.Light.*
-}
 
-deploy_dev() {
-	local DLL=$1.dll
-
-	if [ -f "./bin/Release/$DLL" ] ; then
-		cp "./bin/Release/$DLL" "$LIB"
+	if [[ ! -d "./bin/Release" && ! -d "./bin/Debug" ]] ; then
+		echo "Absent Release and Debug dirs. Nothingn to do! Rebuild!"
+		exit -2
 	fi
 }
 
 deploy() {
 	local DLL=$1.dll
 
-	if [ -f "./bin/Release/$DLL" ] ; then
-		cp "./bin/Release/$DLL" "./GameData/$TARGETBINDIR/"
-		if [ -d "${KSP_DEV}/GameData/$TARGETBINDIR/" ] ; then
-			cp "./bin/Release/$DLL" "${KSP_DEV}/GameData/$TARGETBINDIR/"
+	if [ -f "./bin/Release/${DLL}" ] ; then
+		cp "./bin/Release/${DLL}" "./GameData/${TARGETBINDIR}/"
+		if [ -d "${KSP_DEV}/GameData/${TARGETBINDIR}/" ] ; then
+			cp "./bin/Release/${DLL}" "${KSP_DEV}/GameData/${TARGETBINDIR}/"
 		fi
 	fi
-	if [ -f "./bin/Debug/$DLL" ] ; then
-		if [ -d "${KSP_DEV}/GameData/$TARGETBINDIR/" ] ; then
-			cp "./bin/Debug/$DLL" "${KSP_DEV}/GameData/$TARGETBINDIR/"
+	if [ -f "./bin/Debug/${DLL}" ] ; then
+		if [ -d "${KSP_DEV}/GameData/${TARGETBINDIR}/" ] ; then
+			cp "./bin/Debug/${DLL}" "${KSP_DEV}/GameData/${TARGETBINDIR}/"
+		fi
+	fi
+}
+
+deploy_lib() {
+	local DLL=$1.dll
+
+	if [ -f "./bin/Release/${DLL}" ] ; then
+		cp "./bin/Release/${DLL}" "${LIB}"
+	fi
+}
+
+deploy_plugindata() {
+	local DLL=$1.dll
+
+	if [ -f "./bin/Release/${DLL}" ] ; then
+		for d in . ${KSP_DEV} ; do
+			cp "./bin/Release/${DLL}" "${d}/GameData/${TARGETBINDIR}/PluginData"
+		done
+	fi
+	if [ -f "./bin/Debug/${DLL}" ] ; then
+		if [ ! -z ${KSP_DEV} ] ; then
+			cp "./bin/Debug/${DLL}" "${KSP_DEV}/GameData/${TARGETBINDIR}/PluginData"
+		fi
+	fi
+}
+
+deploy_plugindata_sub() {
+	local DLL=$1.dll
+	local TARGET=${PD_SUB_RULES[$1]}.dll
+
+	if [ -f "./bin/Release/${DLL}" ] ; then
+		for d in . ${KSP_DEV} ; do
+			cp "./bin/Release/${DLL}" "${d}/GameData/${TARGETBINDIR}/PluginData/${TARGET}"
+		done
+	fi
+	if [ -f "./bin/Debug/${DLL}" ] ; then
+		if [ ! -z ${KSP_DEV} ] ; then
+			cp "./bin/Debug/${DLL}" "${KSP_DEV}/GameData/${TARGETBINDIR}/PluginData/${TARGET}"
 		fi
 	fi
 }
@@ -39,54 +89,46 @@ deploy_gamedata() {
 	local PLACE=$1
 	local DLL=$2.dll
 
-	if [ -f "./bin/Release/$DLL" ] ; then
-		cp "./bin/Release/$DLL" "./GameData/000_$DLL"
-		if [ -d "${KSP_DEV}/GameData/" ] ; then
-			cp "./bin/Release/$DLL" "${KSP_DEV}/GameData/${PLACE}_$DLL"
-		fi
+	if [ -f "./bin/Release/${DLL}" ] ; then
+		for d in . ${KSP_DEV} ; do
+			cp "./bin/Release/${DLL}" "${d}/GameData/${PLACE}_${DLL}"
+		done
 	fi
-	if [ -f "./bin/Debug/$DLL" ] ; then
-		if [ -d "${KSP_DEV}/GameData/" ] ; then
-			cp "./bin/Debug/$DLL" "${KSP_DEV}/GameData/${PLACE}_$DLL"
-		fi
-	fi
-}
-
-deploy_plugindata() {
-	local DLL=$1.dll
-
-	if [ -f "./bin/Release/$DLL" ] ; then
-		cp "./bin/Release/$DLL" "./GameData/$TARGETBINDIR/PluginData"
-		if [ -d "${KSP_DEV}/GameData/$TARGETBINDIR/PluginData" ] ; then
-			cp "./bin/Release/$DLL" "${KSP_DEV}/GameData/$TARGETBINDIR/PluginData"
-		fi
-	fi
-	if [ -f "./bin/Debug/$DLL" ] ; then
-		if [ -d "${KSP_DEV}/GameData/$TARGETBINDIR/PluginData" ] ; then
-			cp "./bin/Debug/$DLL" "${KSP_DEV}/GameData/$TARGETBINDIR/PluginData"
+	if [ -f "./bin/Debug/${DLL}" ] ; then
+		if [ ! -z ${KSP_DEV} ] ; then
+			cp "./bin/Debug/${DLL}" "${KSP_DEV}/GameData/${PLACE}_$DLL"
 		fi
 	fi
 }
 
-VERSIONFILE=$PACKAGE.version
+VERSIONFILE=${PACKAGE}.version
 
 check
-cp $VERSIONFILE "./GameData/$TARGETDIR"
-cp CHANGE_LOG.md "./GameData/$TARGETDIR"
-cp README.md  "./GameData/$TARGETDIR"
-cp LICENSE* "./GameData/$TARGETDIR"
-cp NOTICE "./GameData/$TARGETDIR"
+cp $VERSIONFILE "./GameData/${TARGETDIR}"
+cp CHANGE_LOG.md "./GameData/${TARGETDIR}"
+cp README.md  "./GameData/${TARGETDIR}"
+cp LICENSE* "./GameData/${TARGETDIR}"
+cp NOTICE "./GameData/${TARGETDIR}"
+
+
+for dll in $LIB_DLLS ; do
+    deploy_lib $dll
+done
+
+for dll in $DLLS ; do
+    deploy $dll
+done
 
 for dll in $PD_DLLS ; do
     deploy_plugindata $dll
 done
 
+for dll in ${!PD_SUB_RULES[@]} ; do
+    deploy_plugindata_sub $dll
+done
+
 for dll in $GD_DLLS ; do
-    deploy_dev $dll
     deploy_gamedata $GD_PRIORITY $dll
 done
 
-for dll in $DLLS ; do
-    deploy_dev $dll
-    deploy $dll
-done
+echo "${VERSION} Deployed into ${KSP_DEV}"
