d8a40e5239875b9b0cb940050b4b91a1276a16f4
Lisias T <me@lisias.net>
2022-06-26 19:24:41 -0300

    Useless (until now) tentatives on controlling the Toolbar Life Cycle in a saner way.

--- Source/KSPe.UI/Toolbar.cs
+++ Source/KSPe.UI/Toolbar.cs
@@ -721,28 +721,32 @@ namespace KSPe.UI.Toolbar
 
 		internal void start()
 		{
-			if (this.IsVisibleOnCurrentScene())
-			{	// Prevents the button from being displayed if the ApplicationLauncher was already initialized!
-				ApplicationLauncherButton applicationLauncherButton = this.createApplicationLauncher();
-				Log.assert(() => null != applicationLauncherButton, "ApplicationLauncherButton must be NOT NULL");
-				if (null == applicationLauncherButton) return;
-				this.initHandlers(applicationLauncherButton);
-			}
+			ApplicationLauncherButton applicationLauncherButton = this.createApplicationLauncher();
+			Log.assert(() => null != applicationLauncherButton, "ApplicationLauncherButton must be NOT NULL");
+			if (null == applicationLauncherButton) return;
+			this.initHandlers(applicationLauncherButton);
+
+			//// Prevents the button from being displayed if the ApplicationLauncher was already initialized!
+			applicationLauncherButton.Enable(!ApplicationLauncher.Instance.DetermineVisibility(applicationLauncherButton));
+
+				//applicationLauncherButton.enabled = this.IsVisibleOnCurrentScene();
 			this.state.init();
 		}
 
-		private bool IsVisibleOnCurrentScene()
-		{
-			bool r = this.visibleInScenes != ApplicationLauncher.AppScenes.NEVER;
-			r &= this.visibleInScenes == ApplicationLauncher.AppScenes.ALWAYS;
-			r &= GameScenes.MAINMENU == HighLogic.LoadedScene && 0 != (this.visibleInScenes & ApplicationLauncher.AppScenes.MAINMENU);
-			r &= GameScenes.SPACECENTER == HighLogic.LoadedScene && 0 != (this.visibleInScenes & ApplicationLauncher.AppScenes.SPACECENTER);
-			r &= GameScenes.TRACKSTATION == HighLogic.LoadedScene && 0 != (this.visibleInScenes & ApplicationLauncher.AppScenes.TRACKSTATION);
-			r &= HighLogic.LoadedSceneIsEditor && 0 != (this.visibleInScenes & (ApplicationLauncher.AppScenes.SPH | ApplicationLauncher.AppScenes.VAB));
-			r &= HighLogic.LoadedSceneIsFlight && 0 != (this.visibleInScenes & ApplicationLauncher.AppScenes.FLIGHT);
-			r &= (HighLogic.LoadedSceneIsFlight && MapView.MapIsEnabled) && 0 != (this.visibleInScenes & ApplicationLauncher.AppScenes.MAPVIEW);
-			return r;
-		}
+		//private const ApplicationLauncher.AppScenes ApplicationLauncher_AppScenes_EDITOR = ApplicationLauncher.AppScenes.SPH | ApplicationLauncher.AppScenes.VAB;
+		//private bool IsVisibleOnCurrentScene()
+		//{
+		//	return false;
+		//	// ApplicationLauncher.AppScenes.NEVER is Zero. None of the bitwise operations below will result in 1
+		//	// ApplicationLauncher.AppScenes.ALWAYS is -1 (or 0xFF...F). All the bitwise operations below will result in 1!
+		//	bool r = GameScenes.MAINMENU == HighLogic.LoadedScene && 0 != (this.visibleInScenes & ApplicationLauncher.AppScenes.MAINMENU);
+		//	r |= GameScenes.SPACECENTER == HighLogic.LoadedScene && 0 != (this.visibleInScenes & ApplicationLauncher.AppScenes.SPACECENTER);
+		//	r |= GameScenes.TRACKSTATION == HighLogic.LoadedScene && 0 != (this.visibleInScenes & ApplicationLauncher.AppScenes.TRACKSTATION);
+		//	r |= HighLogic.LoadedSceneIsEditor && 0 != (this.visibleInScenes & ApplicationLauncher_AppScenes_EDITOR);
+		//	r |= HighLogic.LoadedSceneIsFlight && 0 != (this.visibleInScenes & ApplicationLauncher.AppScenes.FLIGHT);
+		//	r |= (HighLogic.LoadedSceneIsFlight && MapView.MapIsEnabled) && 0 != (this.visibleInScenes & ApplicationLauncher.AppScenes.MAPVIEW);
+		//	return r;
+		//}
 
 		private ApplicationLauncherButton createApplicationLauncher()
 		{
