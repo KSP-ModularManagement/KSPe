279319612ca4dbf0ccf5b4b8246531ddb69247d9
Lisias T <me@lisias.net>
2022-09-12 17:30:32 -0300

    External tool (console) to check KSP's health (and probably some new interesting things)

---
+++ Source/ksp-offline-check/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+	This file is part of KSPe, a component for KSP API Extensions/L
+		© 2018-22 LisiasT : http://lisias.net <support@lisias.net>
+
+	KSPe API Extensions/L is double licensed, as follows:
+		* SKL 1.0 : https://ksp.lisias.net/SKL-1_0.txt
+		* GPL 2.0 : https://www.gnu.org/licenses/gpl-2.0.txt
+
+	And you are allowed to choose the License that better suit your needs.
+
+	KSPe API Extensions/L is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	You should have received a copy of the SKL Standard License 1.0
+	along with KSPe API Extensions/L. If not, see <https://ksp.lisias.net/SKL-1_0.txt>.
+
+	You should have received a copy of the GNU General Public License 2.0
+	along with KSPe API Extensions/L. If not, see <https://www.gnu.org/licenses/>.
+
+*/
+using System;
+
+namespace kspofflinecheck
+{
+	static class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("KSPe's Console Tool for KSP v{0}", KSPe.Version.Text);
+			if (0 == args.Length) Print_Help_and_Exit(0);
+			switch (args[0])
+			{
+				case "DLL": Checks.DLL.Do(GetParms(args)); break;
+				default:
+					Console.Error.WriteLine("Unrecognized command {0}!", args[0]);
+					Print_Help_and_Exit(-1);
+					break;
+			}
+		}
+
+		private static void Print_Help_and_Exit(int exitCode)
+		{
+			// TODO
+			System.Environment.Exit(exitCode);
+		}
+
+		private static string[] GetParms(string[] args)
+		{
+			string[] r = new string[args.Length-1];
+			Array.Copy(args, 1, r, 0, r.Length);
+			return r;
+		}
+
+	}
+}
---
+++ Source/ksp-offline-check/util/Misc.cs
@@ -0,0 +1,39 @@
+﻿/*
+	This file is part of KSPe, a component for KSP API Extensions/L
+		© 2018-22 LisiasT : http://lisias.net <support@lisias.net>
+
+	KSPe API Extensions/L is double licensed, as follows:
+		* SKL 1.0 : https://ksp.lisias.net/SKL-1_0.txt
+		* GPL 2.0 : https://www.gnu.org/licenses/gpl-2.0.txt
+
+	And you are allowed to choose the License that better suit your needs.
+
+	KSPe API Extensions/L is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	You should have received a copy of the SKL Standard License 1.0
+	along with KSPe API Extensions/L. If not, see <https://ksp.lisias.net/SKL-1_0.txt>.
+
+	You should have received a copy of the GNU General Public License 2.0
+	along with KSPe API Extensions/L. If not, see <https://www.gnu.org/licenses/>.
+
+*/
+using System;
+namespace kspofflinecheck.util
+{
+	internal static class Util
+	{
+		public static void Print_Warning(string fmt, params object[] @params)
+		{
+			Console.WriteLine("Warning: " + fmt, @params);
+		}
+
+		public static void Print_Error_and_Exit(int exitCode, string fmt, params object[] @params)
+		{
+			Console.Error.WriteLine(fmt, @params);
+			Console.Error.WriteLine("Quitting with {0}...", exitCode);
+			System.Environment.Exit(exitCode);
+		}
+	}
+}
