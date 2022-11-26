4c8d055f7e10ad7bd72b5bfbfc9c0c445a229657
Lisias T <me@lisias.net>
2022-11-26 02:25:31 -0300

    Slightly more diagnosable sanity checks.

--- Source/KSPe/SanityChecks.cs
+++ Source/KSPe/SanityChecks.cs
@@ -36,8 +36,13 @@ namespace KSPe
 			string pwd = KSPe.IO.Directory.GetCurrentDirectory();
 			string origin = KSPe.IO.Path.Origin();
 
-			Log.detail("pwd:                        {0}", pwd);
-			Log.detail("origin:                     {0}", origin);
+			Log.detail("System.IO.Directory.GetCurrentDirectory: {0}", System.IO.Directory.GetCurrentDirectory());
+			Log.detail("KSPe.IO.Directory.GetCurrentDirectory:   {0}", pwd);
+			Log.detail("KSPe.IO.Path.Origin:                     {0}", origin);
+
+			// KSPe will not run on this scenario, even if theoretically possible - KSP itself doesn't behaves, so there's no point
+			// (and it would be dangerous) on proceeding. There're suicidal users out there willing to try this, but I'm not going
+			// to put myself on a situation in which I could prevent a disaster and did nothing about.
 
 			// Naivelly comparing the paths is borking on Windows, as this thingy uses case insensity pathnames by default.
 			// if (!pwd.Equals(origin)) FatalErrors.PwdIsNotOrigin.Show(pwd, origin);
@@ -60,16 +65,17 @@ namespace KSPe
 
 			Log.detail("KSPUtil.ApplicationRootPath {0}", KSPUtil.ApplicationRootPath);
 			Log.detail("KSPe.IO.Path.AppRoot        {0}", app_root);
-			Log.detail("origin:                     {0}", origin);
+			Log.detail("KSPe.IO.Path.Origin:        {0}", origin);
 
 			// On KSP 1.12.4, PD made a bad move on the PD Launcher that ended up users trying to get rid of it anyway they could.
+			// This ended up with a screwed KSP rig, as the pwd is now on the PD-Launcher directory, and not on KSP's anymore.
 			// See https://forum.kerbalspaceprogram.com/index.php?/topic/210419-get-rid-of-the-stupid-launcher-nobody-likes-them-and-they-do-nothing-but-ruin-the-gaming-experience/&do=findComment&comment=4196378
 			// for the gory details.
 			{
 				Uri uri_origin = new Uri(System.IO.Path.Combine(origin, "dummy.txt"));
 				Uri uri_approot = new Uri(System.IO.Path.Combine(app_root, "dummy.txt"));
 
-				if (uri_approot != uri_origin) FatalErrors.ApplicationRootPathIsNotOrigin.Show(KSPUtil.ApplicationRootPath, origin);
+				if (!uri_approot.Equals(uri_origin)) FatalErrors.ApplicationRootPathIsNotOrigin.Show(KSPUtil.ApplicationRootPath, origin);
 			}
 		}
 	}
