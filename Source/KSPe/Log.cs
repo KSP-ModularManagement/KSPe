dc441c8b85558e39fcfb54f817220701a98c079e
Lisias T <me@lisias.net>
2023-05-07 23:07:45 -0300

    Adding stackdump support to the UI's Log.

--- Source/KSPe/Log.cs
+++ Source/KSPe/Log.cs
@@ -23,50 +23,17 @@ namespace KSPe
 	{
 		private static readonly Logger log = Logger.CreateForType<Startup>(false, 1);
 
-		public static void force (string msg, params object [] @params)
-		{
-			log.force (msg, @params);
-		}
-
-		public static void info(string msg, params object[] @params)
-		{
-			log.info(msg, @params);
-		}
-
-		public static void warn(string msg, params object[] @params)
-		{
-			log.warn(msg, @params);
-		}
-
-		public static void detail(string msg, params object[] @params)
-		{
-			log.detail(msg, @params);
-		}
-
-		public static void trace(string msg, params object[] @params)
-		{
-			log.trace(msg, @params);
-		}
-
-		public static void error(string msg, params object[] @params)
-		{
-			log.error(msg, @params);
-		}
-
-		public static void error(System.Exception e, object offended)
-		{
-			log.error(offended, e);
-		}
-
-		public static void error(System.Exception e, string msg, params object[] @params)
-		{
-			log.error(e, msg, @params);
-		}
+		public static void force (string msg, params object [] @params) => log.force (msg, @params);
+		public static void info(string msg, params object[] @params) => log.info(msg, @params);
+		public static void warn(string msg, params object[] @params) => log.warn(msg, @params);
+		public static void detail(string msg, params object[] @params) => log.detail(msg, @params);
+		public static void trace(string msg, params object[] @params) => log.trace(msg, @params);
+		public static void error(string msg, params object[] @params) => log.error(msg, @params);
+		public static void error(System.Exception e, object offended) => log.error(offended, e);
+		public static void error(System.Exception e, string msg, params object[] @params) => log.error(e, msg, @params);
+		public static void stackDump(object offended) => log.stack(offended, true);
 
 		[ConditionalAttribute("DEBUG")]
-		public static void debug(string msg, params object[] @params)
-		{
-			log.trace(msg, @params);
-		}
+		public static void debug(string msg, params object[] @params) => log.trace(msg, @params);
 	}
 }
