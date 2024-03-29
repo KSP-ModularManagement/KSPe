c0b0a19d07e4ed9206516d53be31a034cc510533
Lisias T <me@lisias.net>
2024-03-29 16:38:49 -0300

    Bump the install checker version to force an update.

--- Source/InstallChecker/Properties/MyVersion.cs
+++ Source/InstallChecker/Properties/MyVersion.cs
@@ -6,11 +6,11 @@ namespace KSPe.InstallChecker
 	public static class Version
 	{
 		public const int major = 1;
-		public const int minor = 0;
+		public const int minor = 1;
 		public const int patch = 0;
 		public const int build = 0;
 		public static readonly System.Version V = new System.Version(major, minor, patch, build);
-		public const string Number = "1.0.0.0";
+		public const string Number = "1.1.0.0";
 #if DEBUG
 		public const string Text = Number + " /L DEBUG";
 #else
